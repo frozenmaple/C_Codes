41805ddd3f92249087a46eec47d7965f773e7c37
Feng Qian <frozenmaple@126.com>
2020-10-10 21:27:40 +0800

    :droplet:clean commit history

--- test_demo_1.c
+++ test_demo_1.c
@@ -1,3 +1,14 @@
+/*
+** test_demo_1.c for test_demo in /mnt/d/Codes/C_Lang
+**
+** Made by frozenmaple
+** Login   <frozenmaple>
+**
+** Started on  Sat Oct 10 9:24:56 PM 2020 frozenmaple
+** Last update Sun Oct 10 9:25:08 PM 2020 frozenmaple
+*/
+
+
 #include<stdio.h>
 int main()
 {
